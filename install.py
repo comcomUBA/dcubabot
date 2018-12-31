14e0548ea3b5221d01893d2e55d9a88c86b4a38a
Damián Garro <dgarro@dc.uba.ar>
2018-12-31 04:00:17 -0300

    Bot Install: Listable entries deleted

--- install.py
+++ install.py
@@ -15,14 +15,3 @@ with db_session:
     Command(name="listarotro", description="Muestra todos los grupos relacionados a la gente de este grupo "
                                            "(algo así como off-topics)")
     Command(name="cubawiki")
-
-    #TODO: Delete this
-    Obligatoria.select().delete()
-    Obligatoria(name="Sistemas", url="https://t.me/joinchat/CDIKsEAaiqgMpr47cW0I1Q", chat_id=-1)
-    Obligatoria(name="Orga 1", url="https://t.me/joinchat/DS9ZukGbZgIOIaHgdBlavQ", chat_id=-1)
-    Obligatoria(name="Orga 2", url="https://t.me/joinchat/Cy7kt0RKuAeYfsyffvTR2A", chat_id=-1)
-    Obligatoria(name="Álgebra", url="https://t.me/algebra1dmuba", chat_id=-1)
-    Optativa.select().delete()
-    Optativa(name="PAP", url="https://telegram.me/PapDCUBA", chat_id=-1)
-    Otro.select().delete()
-    Otro(name="SPAM", url="https://t.me/joinchat/CDIKsEORDzVL5S2z2YwUQA", chat_id=-1)
