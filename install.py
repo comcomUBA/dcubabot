ec2d43928a1116ad2d9512f2c5069665fb5a063a
uriel <rozenberguriel@gmail.com>
2019-07-06 21:59:01 -0300

    Bot code: Install spaces to tabs

--- install.py
+++ install.py
@@ -6,39 +6,39 @@ from models import *
 
 
 def install_commands():
-    with db_session:
-        Command(name="start")
-        Command(name="help", description="Muestra este mensaje horrible")
-        Command(name="estasvivo",
-                description="Responde un mensaje corto para ver si el bot esta al día y activo")
-        Command(name="listar",
-                description="Muestra todos los grupos de materias obligatorias conocidos por el bot")
-        Command(name="listaroptativa",
-                description="Muestra todos los grupos de materias optativas conocidos por el bot")
-        Command(name="listarotro", description="Muestra todos los grupos relacionados a la gente de este grupo "
-                                               "(algo así como off-topics)")
-        Command(name="listarlabos", description="Lista las reservaciones de los laboratorios de la facultad")
-        Command(name="cubawiki")
-        Command(name="noitip", description="Escuchar un tip de noit de orga2")
-        Command(name="asm", description="Información sobre una instrucción de Intel 64 o IA-32")
-        Command(name="sugerirgrupo",
-                description="Sugiere un grupo de alguna de las materias obligatorias")
-        Command(name="sugeriroptativa",
-                description="Sugiere un grupo de alguna de las materias optativas")
-        Command(name="sugerirotro",
-                description="Sugiere un grupo de cualquier cosa donde predomine gente de Exactas")
-        Command(name="flan",
-                description="Muestra el grafo de materias de la carrera con correlatividades")
+	with db_session:
+		Command(name="start")
+		Command(name="help", description="Muestra este mensaje horrible")
+		Command(name="estasvivo",
+				description="Responde un mensaje corto para ver si el bot esta al día y activo")
+		Command(name="listar",
+				description="Muestra todos los grupos de materias obligatorias conocidos por el bot")
+		Command(name="listaroptativa",
+				description="Muestra todos los grupos de materias optativas conocidos por el bot")
+		Command(name="listarotro", description="Muestra todos los grupos relacionados a la gente de este grupo "
+											   "(algo así como off-topics)")
+		Command(name="listarlabos", description="Lista las reservaciones de los laboratorios de la facultad")
+		Command(name="cubawiki")
+		Command(name="noitip", description="Escuchar un tip de noit de orga2")
+		Command(name="asm", description="Información sobre una instrucción de Intel 64 o IA-32")
+		Command(name="sugerirgrupo",
+				description="Sugiere un grupo de alguna de las materias obligatorias")
+		Command(name="sugeriroptativa",
+				description="Sugiere un grupo de alguna de las materias optativas")
+		Command(name="sugerirotro",
+				description="Sugiere un grupo de cualquier cosa donde predomine gente de Exactas")
+		Command(name="flan",
+				description="Muestra el grafo de materias de la carrera con correlatividades")
 		Command(name="sugerir")		
-        Command(name="sugerirNoticia")
+		Command(name="sugerirNoticia")
 
-        # Administration commands
-        Command(name="togglecommand")
+		# Administration commands
+		Command(name="togglecommand")
 
 
 
 if __name__ == '__main__':
-    db.bind('sqlite', "dcubabot.sqlite3", create_db=True)
-    db.drop_table("Command",if_exists=True, with_all_data=True)
-    db.generate_mapping(create_tables=True)
-    install_commands()
+	db.bind('sqlite', "dcubabot.sqlite3", create_db=True)
+	db.drop_table("Command",if_exists=True, with_all_data=True)
+	db.generate_mapping(create_tables=True)
+	install_commands()
