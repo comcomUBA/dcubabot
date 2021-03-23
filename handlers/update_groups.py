4c7ebe766f386fb7744e5a9c89aae32893c00a90
uriel <urielrozenberg@hotmail.com>
2021-03-23 06:55:40 -0300

    probando actualizar grupos

--- handlers/update_groups.py
+++ handlers/update_groups.py
@@ -18,21 +18,22 @@ def update_group_url(context: CallbackContext, chat_id: str) -> (str, str, bool)
 def update_groups(context: CallbackContext):
     print("voy a actualizar grupos")
     with db_session:
-        chats = list(select((l.id, l.name) for l in Listable if l.validated))
+        chats = list(select((l.id, l.chat_id, l.name) for l in Listable if l.validated))
     print(f"voy a actualizar {chats}")
-    for (chat_id, url, validated), name in [(update_group_url(context, chat_id), name) for chat_id, name in chats]:
+    for id, (chat_id, url, validated), name in [(update_group_url(context, chat_id), name) for id, chat_id, name in
+                                                chats]:
         print(f"lo que va pasar es que {((chat_id, url, validated), name)} ")
         if not validated:
             with db_session:
                 print("mira q lo borro")
-                Listable[chat_id].validated = False
+                Listable[id].validated = False
                 print("lo borre")
             print("mira q lo mando")
             context.bot.send_message(chat_id="-1001067544716", text=f"El grupo {name} murió 💀")
             print("lo mandé")
         else:
             with db_session:
-                Listable[chat_id].url = url
+                Listable[id].url = url
 
 
 def actualizar_grupos(update: Update, context: CallbackContext):
