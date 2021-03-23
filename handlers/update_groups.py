af34eefcf694c6a4229fe844add599fb473884da
uriel <urielrozenberg@hotmail.com>
2021-03-23 06:22:56 -0300

    ups habia sacado noitips

--- handlers/update_groups.py
+++ handlers/update_groups.py
@@ -17,7 +17,7 @@ def update_group_url(context: CallbackContext, chat_id: str) -> (str, str, bool)
 
 def update_groups(context: CallbackContext):
     with db_session:
-        chats = select((l.id, l.name) for l in Listable if l.validated)
+        chats = list(select((l.id, l.name) for l in Listable if l.validated))
     for (chat_id, url, validated), name in [(update_group_url(context, chat_id), name) for chat_id, name in chats]:
         with db_session:
             if not validated:
