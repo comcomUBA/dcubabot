4308a95de2ca133da3ba07572ef4933eb487838c
uriel <urielrozenberg@hotmail.com>
2021-03-23 06:31:44 -0300

    probando actualizar grupos

--- handlers/update_groups.py
+++ handlers/update_groups.py
@@ -16,9 +16,12 @@ def update_group_url(context: CallbackContext, chat_id: str) -> (str, str, bool)
 
 
 def update_groups(context: CallbackContext):
+    print("voy a actualizar grupos")
     with db_session:
         chats = list(select((l.id, l.name) for l in Listable if l.validated))
+    print(f"voy a actualizar {chats}")
     for (chat_id, url, validated), name in [(update_group_url(context, chat_id), name) for chat_id, name in chats]:
+        print(f"lo que va pasar es que {((chat_id, url, validated), name)} ")
         with db_session:
             if not validated:
                 Listable[chat_id].validated = False
