fe8a83b0365de8b90537a2aee2b61b25d6bba532
CamiKaseM7 <camilorivasramunni@gmail.com>
2023-11-11 04:12:22 -0300

    Enviar mensaje cuando hay concierto en River

--- dcubabot.py
+++ dcubabot.py
@@ -349,14 +349,14 @@ def button(update, context):
                                         text=message.text + action_text)
 
 
-def actualizarRiver(context):
+def actualizarPartidos(context):
     hoy = datetime.datetime.now()
     mañana = hoy + datetime.timedelta(days=1)
     local, partido = river.es_local(mañana)
     if not local:
         return
 
-    def river_msg(context):
+    def partido_msg(context):
         if partido.hora is None:
             horario = "hora a confirmar"
         else:
@@ -369,11 +369,29 @@ def actualizarRiver(context):
 
     # la hora local es UTC así que especificamos el timezone que corresponde para el aviso acá
     avisoHora = hoy.replace(hour=20, tzinfo=bsasTz)  # 8pm argentina, buenos aires
-    context.job_queue.run_once(callback=river_msg, when=avisoHora)
+    context.job_queue.run_once(callback=partido_msg, when=avisoHora)
 
     # para testearlo
-    # river_msg(context)
+    # partido_msg(context)
 
+def actualizarConciertos(context):
+    hoy = datetime.datetime.now()
+    mañana = hoy + datetime.timedelta(days=1)
+    hay_concierto, concierto = river.hay_concierto(mañana)
+    
+    if not hay_concierto:
+        return
+    
+    def concierto_msg(context):
+        msg = f"Mañana hay un concierto en River\n{concierto.titulo}"
+        context.bot.sendMessage(chat_id=NOTICIAS_CHATID, text=msg)
+        
+    avisoHora = hoy.replace(hour=20, tzinfo=bsasTz)  # 8pm argentina, buenos aires
+    context.job_queue.run_once(callback=concierto_msg, when=avisoHora)
+
+def actualizarRiver(context):
+    actualizarPartidos(context)
+    actualizarConciertos(context)
 
 def add_all_handlers(dispatcher):
     descriptions = []
--- river.py
+++ river.py
@@ -1,8 +1,20 @@
 import datetime
+import re
 
 from dataclasses import dataclass
 
 from robobrowser import RoboBrowser
+
+# https://stackoverflow.com/questions/35226904/convert-spanish-date-in-string-format
+# Si google responde con los meses en ingles entonces esto no es necesario
+# Desde el codigo me responde con Dic en lugar de Dec como en el navegador
+# Si no, hay que instalar el locale dependiendo de la distro
+
+# sudo locale-gen es_ES.UTF-8; sudo update-locale         # Ubuntu
+# https://wiki.archlinux.org/title/locale                 # Arch
+
+import locale
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
 RIVER = "River Plate"
 UNSPECIFIED_TIMES = {"A confirmar", ""}
@@ -21,7 +33,7 @@ class Partido:
         return self.equipo_local == RIVER
 
     @staticmethod
-    def from_d_calendario(el):
+    def parse(el):
         equipos = [e.strip() for e in el.select("b")[0].text.split("vs.")]
         line2 = el.select("p")[0].text
         copa, _, line2 = line2.partition(" • ")
@@ -51,24 +63,68 @@ class Partido:
             fecha,
             hora,
         )
+    
+@dataclass
+class Concierto:
+    titulo: str
+    fecha: datetime.date
+    
+    @staticmethod
+    def parse(evento):
+        # Esto es muy feo, si llegan a cambiar algo cagamos...
+        dia = evento.select(".UIaQzd")[0].text
+        mes = evento.select(".wsnHcb")[0].text
+        titulo = evento.select(".YOGjf")[0].text
+        lugar = evento.select(".zvDXNd")[0].text.lower()
 
+        hoy = datetime.now().date() 
+        anio = hoy.year # Asumimos que es de este año
+        fecha = datetime.strptime(f"{dia} {mes} {anio}", "%d %b %Y").date()
 
-def fetch():
+        # Puede que nos de cualquier cosa la busqueda
+        es_en_river = re.search(r'(Monumental|Alcorta 7597)', lugar, re.IGNORECASE)
+        
+        if not es_en_river:
+            return None
+        
+        # Si el titulo tiene "vs" entonces es un partido de futbol (heuristica)
+        # y ya lo cubre la otra funcion
+        if "vs" in titulo.lower():
+            return None
+        
+        return Concierto(
+            titulo,
+            fecha,
+        )
+
+def fetch_partidos():
     browser = RoboBrowser(parser="html.parser")
     browser.open("https://www.cariverplate.com.ar/calendario-de-partidos")
 
-    res = []
+    partidos = []
 
     for el in browser.select(".d_calendario"):
-        res.append(Partido.from_d_calendario(el))
+        partidos.append(Partido.parse(el))
+        
+    return partidos
 
-    return res
+def fetch_conciertos():
+    browser = RoboBrowser(parser="html.parser", user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0')
 
+    query = "Conciertos Av. Pres. Figueroa Alcorta 7597"
+    browser.open(f"https://www.google.com/search?q={query}&ibp=htl;events")
+    
+    conciertos = []
+    
+    for el in browser.select(".gws-horizon-textlists__tl-lif"):
+        conciertos.append(Concierto.parse(el))
+    
+    return conciertos
 
 def es_local(dt: datetime.datetime):
     fecha = dt.date()
 
-    partidos = fetch()
+    partidos = fetch_partidos()
 
     for p in partidos:
         if p.fecha != fecha:
@@ -81,8 +137,19 @@ def es_local(dt: datetime.datetime):
 
     return False, None
 
+def hay_concierto(dt: datetime.datetime):
+    fecha = dt.date()
 
+    conciertos = fetch_conciertos()
+
+    for c in conciertos:
+        if c.fecha != fecha:
+            continue
+
+        return True, c
+
+    return False, None
+    
 if __name__ == "__main__":
-    partidos = fetch()
-
     print(es_local(datetime.datetime.today()))
+    print(hay_concierto(datetime.datetime.today()))
