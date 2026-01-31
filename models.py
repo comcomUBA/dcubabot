dc54d9fcf50b671da83bac99176cc01eab3a3101
uriel <uriel.rozenberg@modak.live>
2026-01-31 18:06:03 -0300

    wip

--- models.py
+++ models.py
@@ -1,79 +1,139 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from pony.orm import *
+import os
 import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, Date, Text
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.types import BigInteger
 
-db = Database()
+# --- Database Setup ---
+Base = declarative_base()
+engine = None
+Session = None
 
+def init_db():
+    global engine, Session
+    db_url = "cockroachdb://{user}:{password}@{host}:{port}/{database}".format(
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        database=os.environ["DB_NAME"]
+    )
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-class Command(db.Entity):
-    name = Required(str)
-    description = Optional(str)
-    enabled = Required(bool, default=True)
+# --- Model Definitions ---
 
+class Command(Base):
+    __tablename__ = 'commands'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    description = Column(String)
+    enabled = Column(Boolean, nullable=False, default=True)
 
-class SentMessage(db.Entity):
-    command = Required(str)
-    chat_id = Required(int, size=64)
-    message_id = Required(int, size=64)
-    timestamp = Required(datetime.datetime, default=datetime.datetime.utcnow)
+class SentMessage(Base):
+    __tablename__ = 'sent_messages'
+    id = Column(Integer, primary_key=True)
+    command = Column(String, nullable=False)
+    chat_id = Column(BigInteger, nullable=False)
+    message_id = Column(BigInteger, nullable=False)
+    timestamp = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
 
+class Listable(Base):
+    __tablename__ = 'listables'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    chat_id = Column(String)
+    validated = Column(Boolean, default=False)
+    type = Column(String(50))
 
-class Listable(db.Entity):
-    name = Required(str)
-    url = Required(str)
-    chat_id = Optional(str)
-    validated = Required(bool, default=False)
-
+    __mapper_args__ = {
+        'polymorphic_identity': 'listable',
+        'polymorphic_on': type
+    }
 
 class Obligatoria(Listable):
-    cubawiki_url = Optional(str)
-
+    __tablename__ = 'obligatorias'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    cubawiki_url = Column(String)
+    __mapper_args__ = {
+        'polymorphic_identity': 'obligatoria',
+    }
 
 class Optativa(Listable):
-    pass
-
+    __tablename__ = 'optativas'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'optativa',
+    }
 
 class ECI(Listable):
-    pass
-
+    __tablename__ = 'ecis'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'eci',
+    }
 
 class Otro(Listable):
-    pass
-
+    __tablename__ = 'otros'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'otro',
+    }
 
 class Grupo(Listable):
-    pass
+    __tablename__ = 'grupos'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'grupo',
+    }
 
 class GrupoOptativa(Listable):
-    pass
+    __tablename__ = 'grupo_optativas'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'grupo_optativa',
+    }
 
 class GrupoOtros(Listable):
-    pass
+    __tablename__ = 'grupo_otros'
+    id = Column(Integer, primary_key=True)
+    listable_id = Column(Integer, autoincrement=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'grupo_otros',
+    }
 
-# TODO: Subclasificar con validable
-class Noticia(db.Entity):
-    text = Required(str)
-    date = Required(datetime.date, default=datetime.date.today())
-    validado = Required(bool, default=True)
+class Noticia(Base):
+    __tablename__ = 'noticias'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+    date = Column(Date, nullable=False, default=datetime.date.today)
+    validado = Column(Boolean, default=True)
 
+class Noitip(Base):
+    __tablename__ = 'noitips'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
 
-class Noitip(db.Entity):
-    text = Required(str)
+class AsmInstruction(Base):
+    __tablename__ = 'asm_instructions'
+    id = Column(Integer, primary_key=True)
+    mnemonic = Column(String, nullable=False)
+    summary = Column(Text, nullable=False)
+    url = Column(String, nullable=False)
 
-
-class AsmInstruction(db.Entity):
-    mnemonic = Required(str)
-    summary = Required(str)
-    url = Required(str)
-
-
-class File(db.Entity):
-    path = Required(str)
-    file_id = Required(str)
-
-
-def init_db(path):
-    db.bind('sqlite', path, create_db=True)
-    db.generate_mapping(create_tables=True)
+class File(Base):
+    __tablename__ = 'files'
+    id = Column(Integer, primary_key=True)
+    path = Column(String, nullable=False, unique=True)
+    file_id = Column(String, nullable=False)
