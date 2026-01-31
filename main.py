218d51e97a68db2d8d77f4f06099ce598ec27041
uriel <uriel.rozenberg@modak.live>
2026-01-31 16:43:32 -0300

    wip

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ bot = telegram.Bot(os.environ["TELEGRAM_BOT_TOKEN"])
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    bot.sendMessage(chat_id=chat_id, text=update.message.text)
     return "OK"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
